2ab03b4ce3c2ea96eafddddb3def72bcc31dcb15
Adela Knap <adkn2300@student.miun.se>
2025-02-28 10:39:20 +0100

    Skapar grundstrukturen och de pages som behövs initialt

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            <h1>404 - Sidan hittades inte</h1>
+            <p>Oj! Sidan du letar efter finns inte.</p>
+        </div>
+    )
+}
+
+export default NotFoundPage
